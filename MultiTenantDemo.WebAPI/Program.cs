225d191152f3615f574fb58d35b7e66a0ad9eb54
Michael Samorokov <michael.samorokov@gmail.com>
2019-09-03 16:50:47 -0500

    added tenant specific configuration

--- MultiTenantDemo.WebAPI/Program.cs
+++ MultiTenantDemo.WebAPI/Program.cs
@@ -19,6 +19,12 @@ namespace MultiTenantDemo.WebAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(confBuilder =>
+                {
+                    confBuilder.SetBasePath(Directory.GetCurrentDirectory());
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "tenant-configs");
+                    confBuilder.AddKeyPerFile(path, true);
+                })
                 .UseStartup<Startup>();
     }
 }
