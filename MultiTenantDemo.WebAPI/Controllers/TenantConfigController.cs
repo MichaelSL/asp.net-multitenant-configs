6c9b15311c1ea5bc06d3a2302a3eb81d894d0396
Michael Samorokov <michael.samorokov@gmail.com>
2019-09-03 16:45:46 -0500

    Added TenantConfigController

---
+++ MultiTenantDemo.WebAPI/Controllers/TenantConfigController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MultiTenantDemo.WebAPI.Controllers
+{
+    [ApiController]
+    public class TenantConfigController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public TenantConfigController(IConfiguration configuration)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        [HttpGet("api/tenantconfig/{tenantId}")]
+        public ActionResult<string> Get(int tenantId)
+        {
+            var value = configuration.GetValue<string>($"TenantConfigs:Tree:Tenant{tenantId}");
+
+            return value ?? $"can't get config value for tenant '{tenantId}'";
+        }
+    }
+}
